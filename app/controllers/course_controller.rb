1d381f448306f321ea42da56b8c8872fe2e797b4
Max Branzburg <maxbranzburg@gmail.com>
2014-10-24 19:41:45 -0700

    Added views

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,7 +1,14 @@
 class CourseController < ApplicationController
+  
   def index
+  	@courses = Course.all
   end
 
   def show
+  	@course = Course.find(params[:id])
+  	@first_session_url = @course.sessions.first.url
+  	@providor = @course.providor
+  	@instructor = @course.instructor
   end
+  
 end
--- app/controllers/providor_controller.rb
+++ app/controllers/providor_controller.rb
@@ -1,7 +1,11 @@
 class ProvidorController < ApplicationController
   def index
+  	@providors = Providor.all
   end
 
   def show
+  	@providor = Providor.find(params[:id])
+  	@courses = @providor.courses
+  	@course_term = Providor.course_plurality(@courses)
   end
 end
