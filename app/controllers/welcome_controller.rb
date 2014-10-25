b6aa127c7c57da186e485e654a63751fc5302551
Max Branzburg <maxbranzburg@gmail.com>
2014-10-24 19:57:21 -0700

    Updated welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
+  	@course_a = Course.find(104)
+  	@course_b = Course.find(121)
+  	@course_c = Course.find(693)
+  	@course_d = Course.find(174)
   end
 
 end
